2e1319c8123c6c8d3056793cade2033e04cfa0f0
PSalazar99 <136025393+PSalazar99@users.noreply.github.com>
2023-06-16 01:40:06 +0000

    Implementacion de modulos

---
+++ programa.py
@@ -0,0 +1,8 @@
+from matematicas import sumar, restar, multiplicar, dividir 
+from geometria import distancia
+
+print("Suma:", sumar(2,3))
+print("Resta:", restar(4, 2))
+print("Multiplicar:", multiplicar(5,2))
+print("Divicion:", dividir(3, 6))
+print("Distancia:" ,distancia(1,2,4,5))
